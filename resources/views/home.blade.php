03bb81b73b6f3a75004c7c55b34466dafd30b552
ThanuMahee12 <thanumahee440@gmail.com>
2024-12-12 14:55:41 +0530

    feat(Task):created Task Model and Migrations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    <h3>{{ __('Task Management') }}</h3>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +16,40 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <!-- Tasks Table -->
+                    <table class="table table-bordered table-striped table-hover">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($tasks as $task)
+                                <tr>
+                                    <td>{{ $loop->iteration + ($tasks->currentPage() - 1) * $tasks->perPage() }}</td>
+                                    <td>
+                                        @if ($task->user_id === auth()->id())
+                                            <a href="{{ route('tasks.show', $task->id) }}" class="text-primary font-weight-bold">
+                                                {{ $task->title }}
+                                            </a>
+                                        @else
+                                            {{ $task->title }}
+                                        @endif
+                                    </td>
+                                    <td>{{ $task->description }}</td>
+                                    <td>{{ $task->user->name }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    <!-- Pagination Links -->
+                    <div class="d-flex justify-content-center">
+                        {{ $tasks->links() }}
+                    </div>
                 </div>
             </div>
         </div>
