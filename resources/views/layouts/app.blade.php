fc9677764337d7360cc99de8601e577e4ccbea39
ThanuMahee12 <thanumahee440@gmail.com>
2024-12-12 19:38:46 +0530

    refactor(notification):notification Show and updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
@@ -30,12 +33,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <!-- Additional Links (Optional) -->
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -43,18 +45,29 @@
                                 </li>
                             @endif
                         @else
+                            <!-- Notifications Dropdown -->
+                            <li class="nav-item dropdown">
+                                <a id="notificationDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Notifications <span class="badge bg-danger" id="notificationBadge">0</span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="notificationDropdown">
+                                    <ul id="notificationList" class="list-group">
+                                        <li class="list-group-item text-muted">No notifications</li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <!-- User Dropdown -->
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                 document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
@@ -70,5 +83,38 @@
             @yield('content')
         </main>
     </div>
+
+    <!-- Laravel Echo and Pusher -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/laravel-echo/1.11.3/echo.min.js"></script>
+    <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const notificationBadge = document.getElementById('notificationBadge');
+            const notificationList = document.getElementById('notificationList');
+
+            window.Echo.channel('task-channel')
+                .listen('TaskCreated', (e) => {
+                    addNotification(`New Task Created: ${e.task.title}`);
+                })
+                .listen('TaskUpdated', (e) => {
+                    addNotification(`Task Updated: ${e.task.title}`);
+                });
+
+            function addNotification(message) {
+                // Update badge count
+                const count = parseInt(notificationBadge.textContent) || 0;
+                notificationBadge.textContent = count + 1;
+
+                // Update notification list
+                if (notificationList.querySelector('.text-muted')) {
+                    notificationList.innerHTML = ''; // Clear "No notifications" message
+                }
+                const newNotification = document.createElement('li');
+                newNotification.className = 'list-group-item';
+                newNotification.textContent = message;
+                notificationList.prepend(newNotification);
+            }
+        });
+    </script>
 </body>
 </html>
